b7bdb671af6e858a0e0b7a75a3f7ee64973f7939
Андрей Емельянов <andr.emeljanov2014@gmail.com>
2021-03-22 11:56:30 +0500

    Products and users search

---
+++ static/js/search.js
@@ -0,0 +1,14 @@
+let items = document.querySelectorAll('.list-link')
+let input = document.querySelector('.search-input')
+
+
+input.oninput = function (){
+    let value = input.value.toLowerCase();
+    for (let item of items){
+        if (item.textContent.toLowerCase().indexOf(value) === -1){
+            item.classList.add('hidden')
+        } else {
+            item.classList.remove('hidden')
+        }
+    }
+}
