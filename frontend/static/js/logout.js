18b46ff81ef2f15d93ebba9098a63bf0df9e728b
reshxs <andr.emeljanov2014@gmail.com>
2021-05-22 16:13:32 +0500

    logout base url fix

--- frontend/static/js/logout.js
+++ frontend/static/js/logout.js
@@ -1,4 +1,5 @@
 logoutButton = document.querySelector(".logout-button");
+let base_url = window.location.origin;
 
 logoutButton.onclick = async function(){
     console.log("logout")
@@ -8,7 +9,7 @@ logoutButton.onclick = async function(){
         "id": "test"
     };
 
-    let response = await fetch("http://localhost/api/v1/jsonrpc", {
+    let response = await fetch(base_url + "/api/v1/jsonrpc", {
         "method": "POST",
         "body": JSON.stringify(request)
     });
